ce6f41548087135b9b386f33e8f1debfe93faa4c
billhorsman <billhorsman>
2002-09-19 09:43:36 +0000

    jdk patch

---
+++ src/java-jdk1.2/org/logicalcobwebs/proxool/DelegateProxy.java
@@ -0,0 +1,21 @@
+/*
+ * This software is released under the Apache Software Licence. See
+ * package.html for details. The latest version is available at
+ * http://proxool.sourceforge.net
+ */
+package org.logicalcobwebs.proxool;
+
+/**
+ * Dummy class that overwrites the one in the main tree (for JDK 1.3+)
+ * because JDK 1.2 doesn't know anything about Proxy.
+ */
+public class DelegateProxy {
+}
+
+/*
+ Revision history:
+ $Log: DelegateProxy.java,v $
+ Revision 1.1  2002/09/19 09:43:36  billhorsman
+ jdk patch
+
+ */
