01503655f55df467020dfe0f7e55f1eb85a5b814
billhorsman <billhorsman>
2002-10-27 12:05:39 +0000

    early, early draft

---
+++ src/java-ext/org/logicalcobwebs/proxool/VirtoolDriver.java
@@ -0,0 +1,81 @@
+/*
+ * This software is released under the Apache Software Licence. See
+ * package.html for details. The latest version is available at
+ * http://proxool.sourceforge.net
+ */
+package org.logicalcobwebs.proxool;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverPropertyInfo;
+import java.util.Properties;
+import java.util.List;
+
+/**
+ * This class acts as a virtual pool. When you ask it for a connection it
+ * delegates to one of the designated real pools.
+ *
+ * @version $Revision: 1.1 $, $Date: 2002/10/27 12:05:39 $
+ * @author Bill Horsman (bill@logicalcobwebs.co.uk)
+ * @author $Author: billhorsman $ (current maintainer)
+ * @since Proxool 0.5
+ */
+public class VirtoolDriver implements Driver {
+
+    private static final String VIRTOOL = "virtool";
+
+    String[] activePools;
+
+    private int nextPool;
+
+    public Connection connect(String url, Properties info)
+            throws SQLException {
+        String alias = activePools[nextPool];
+
+        // Now we need to move to the next pool. This code isn't ThreadSafe and
+        // we don't want to make it so because it would have a performance
+        // impact.
+
+        return null;
+    }
+
+    public boolean acceptsURL(String url) throws SQLException {
+        return (url.startsWith(VIRTOOL));
+    }
+
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
+            throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
+
+    public int getMajorVersion() {
+        throw new UnsupportedOperationException("This virtual driver doesn't support this operation.");
+    }
+
+    public int getMinorVersion() {
+        throw new UnsupportedOperationException("This virtual driver doesn't support this operation.");
+    }
+
+    public boolean jdbcCompliant() {
+        throw new UnsupportedOperationException("This virtual driver doesn't support this operation.");
+    }
+
+    static {
+        try {
+            DriverManager.registerDriver(new VirtoolDriver());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+}
+
+/*
+ Revision history:
+ $Log: VirtoolDriver.java,v $
+ Revision 1.1  2002/10/27 12:05:39  billhorsman
+ early, early draft
+
+*/
