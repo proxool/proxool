16ac909a59ac2bbf6062cb4213a180df1f5f42c5
billhorsman <billhorsman>
2002-10-29 23:16:28 +0000

    We now do real inserts into the db, not just selects

--- src/java-test/org/logicalcobwebs/proxool/Performance.java
+++ src/java-test/org/logicalcobwebs/proxool/Performance.java
@@ -14,6 +14,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Test what the performance of Proxool is compared to a "theoretical"
@@ -21,7 +22,7 @@ import java.sql.SQLException;
  * as we keep a single threaded model (and because most systems work
  * in a multi-threaded environment is why we need a pool).
  *
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  * @author Bill Horsman (bill@logicalcobwebs.co.uk)
  * @author $Author: billhorsman $ (current maintainer)
  * @since Proxool 0.5
@@ -30,9 +31,13 @@ public class Performance extends TestCase {
 
     private static final Log LOG = LogFactory.getLog(Performance.class);
 
+    private String DIRECT_TABLE = "direct";
+
+    private String PROXOOL_TABLE = "proxool";
+
     private static final int LOOPS = 1000;
 
-    private static final int THREADS = 10;
+    private static final int THREADS = 5;
 
     private static final int MAX_PROXIES = 10000;
 
@@ -47,42 +52,61 @@ public class Performance extends TestCase {
 
     protected void setUp() throws Exception {
         AllTests.globalSetup();
-        TestHelper.setupDatabase();
+        TestHelper.createTable(DIRECT_TABLE);
+        TestHelper.createTable(PROXOOL_TABLE);
     }
 
     protected void tearDown() throws Exception {
-        TestHelper.tearDownDatabase();
+        TestHelper.dropTable(DIRECT_TABLE);
+        TestHelper.dropTable(PROXOOL_TABLE);
         AllTests.globalTeardown();
     }
 
     /**
-     * Test how Proxool matches up to the perfect pool
+     * Test how Proxool matches up to the direct pool
      *
      * @throws SQLException if there is any problem
      * @throws ClassNotFoundException if we can't find a driver
      */
     public void testProxool() throws SQLException, ClassNotFoundException {
 
-        double perfect = getPerfectPoolTime();
+        double direct = getDirectPoolTime();
         double proxool = getProxoolTime();
 
-        LOG.info("Proxool overhead is " + (proxool - perfect) + " milliseconds.");
+        LOG.info("Proxool overhead is " + (proxool - direct) + " milliseconds.");
     }
 
-    private double getPerfectPoolTime() throws SQLException, ClassNotFoundException {
+    private double getDirectPoolTime() {
 
-        LOG.info("Testing perfect pool");
+        long elapsed = 0;
+        try {
+            LOG.info("Testing direct pool");
 
-        Connection c = TestHelper.getDirectConnection();
-        long start = System.currentTimeMillis();
+            Connection[] c = new Connection[THREADS];
+            for (int j = 0; j < THREADS; j++) {
+                c[j] = TestHelper.getDirectConnection();
+            }
 
-        for (int i = 0; i < LOOPS * THREADS; i++) {
-            TestHelper.testConnection(c);
+            long start = System.currentTimeMillis();
+
+            for (int i = 0; i < LOOPS; i++) {
+                for (int j = 0; j < THREADS; j++) {
+                    TestHelper.insertRow(c[j], DIRECT_TABLE);
+                }
+            }
+
+            for (int j = 0; j < THREADS; j++) {
+                c[j].close();
+            }
+
+            int count = TestHelper.getCount(TestHelper.getDirectConnection(), DIRECT_TABLE);
+            assertEquals("Wrong number of rows added", (LOOPS * THREADS), count);
+
+            elapsed = System.currentTimeMillis() - start;
+            LOG.info("Perfect pool added " + (LOOPS * THREADS) + " rows in " + elapsed + " milliseconds.");
+        } catch (Exception e) {
+            LOG.error("Eh?", e);
         }
-        c.close();
-
-        long elapsed = System.currentTimeMillis() - start;
-        LOG.info("Perfect pool tested " + (LOOPS * THREADS) + " connections in " + elapsed + " milliseconds.");
 
         return (elapsed / (double) (LOOPS * THREADS));
     }
@@ -91,7 +115,17 @@ public class Performance extends TestCase {
 
         LOG.info("Testing proxool");
 
-        TestHelper.registerPool(ALIAS_PERFORMANCE);
+        Properties info = TestHelper.buildProperties();
+        info.setProperty("proxool.maximum-connection-count", "10");
+        info.setProperty("proxool.minimum-connection-count", "10");
+        info.setProperty("proxool.prototypes-count", "10");
+        TestHelper.registerPool(ALIAS_PERFORMANCE, info);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            LOG.error("Awoken", e);
+        }
 
         long start = System.currentTimeMillis();
 
@@ -100,15 +134,18 @@ public class Performance extends TestCase {
         for (int i = 0; i < LOOPS; i++) {
             for (int j = 0; j < THREADS; j++) {
                 c[j] = TestHelper.getProxoolConnection(url);
-                TestHelper.testConnection(c[j]);
+                TestHelper.insertRow(c[j], PROXOOL_TABLE);
             }
-            for (int j = 0; j < THREADS; j++) {
-                c[j].close();
+            for (int k = 0; k < THREADS; k++) {
+                c[k].close();
             }
         }
 
+        int count = TestHelper.getCount(TestHelper.getProxoolConnection(url), PROXOOL_TABLE);
+        assertEquals("Wrong number of rows added", (LOOPS * THREADS), count);
+
         long elapsed = System.currentTimeMillis() - start;
-        LOG.info("Proxool tested " + (LOOPS * THREADS) + " connections in " + elapsed + " milliseconds.");
+        LOG.info("Proxool added " + (LOOPS * THREADS) + " rows in " + elapsed + " milliseconds.");
 
         return (elapsed / (double) (LOOPS * THREADS));
 
