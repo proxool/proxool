0569f0e30c12309a74cc8a9cf3fdd0c2d0c20c7c
billhorsman <billhorsman>
2002-12-06 15:57:08 +0000

    fix for proxied statement where Statement interface is not directly implemented.

--- src/java/org/logicalcobwebs/proxool/ProxyFactory.java
+++ src/java/org/logicalcobwebs/proxool/ProxyFactory.java
@@ -6,22 +6,24 @@
 */
 package org.logicalcobwebs.proxool;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.sql.*;
 import java.lang.reflect.Proxy;
 
 /**
  * A central place to build proxy objects ({@link ProxyConnection connections}
  * and {@link ProxyStatement statements}).
  *
- * @version $Revision: 1.5 $, $Date: 2002/12/03 12:24:00 $
+ * @version $Revision: 1.6 $, $Date: 2002/12/06 15:57:08 $
  * @author Bill Horsman (bill@logicalcobwebs.co.uk)
  * @author $Author: billhorsman $ (current maintainer)
  * @since Proxool 0.5
  */
 class ProxyFactory {
+
+    private static final Log LOG = LogFactory.getLog(ProxyFactory.class);
 
     protected static ProxyConnection buildProxyConnection(long id, ConnectionPool connectionPool) throws SQLException {
         Connection realConnection = null;
@@ -50,8 +52,20 @@ class ProxyFactory {
                 proxyConnection);
     }
 
-    protected static Object createProxyStatement(Statement delegate, ConnectionPool connectionPool, ProxyConnection proxyConnection,  String sqlStatement) {
-        return Proxy.newProxyInstance(delegate.getClass().getClassLoader(), delegate.getClass().getInterfaces(), new ProxyStatement(delegate, connectionPool, proxyConnection, sqlStatement));
+    protected static Statement createProxyStatement(Statement delegate, ConnectionPool connectionPool, ProxyConnection proxyConnection,  String sqlStatement) {
+        // We can't use Class#getInterfaces since that doesn't take
+        // into account superclass interfaces. We could, laboriously,
+        // work our way up the hierarchy but it doesn't seem worth while -
+        // we only actually expect three options:
+        Class[] interfaces = new Class[1];
+        if (delegate instanceof CallableStatement) {
+            interfaces[0] = CallableStatement.class;
+        } else if (delegate instanceof PreparedStatement) {
+            interfaces[0] = PreparedStatement.class;
+        } else {
+            interfaces[0] = Statement.class;
+        }
+        return (Statement) Proxy.newProxyInstance(delegate.getClass().getClassLoader(), interfaces, new ProxyStatement(delegate, connectionPool, proxyConnection, sqlStatement));
     }
 
 
@@ -60,6 +74,10 @@ class ProxyFactory {
 /*
  Revision history:
  $Log: ProxyFactory.java,v $
+ Revision 1.6  2002/12/06 15:57:08  billhorsman
+ fix for proxied statement where Statement interface is not directly
+ implemented.
+
  Revision 1.5  2002/12/03 12:24:00  billhorsman
  fixed fatal sql exception
 
