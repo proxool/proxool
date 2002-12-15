8b4c2b2c103c01cd03b4b210139bb54489c764aa
chr32 <chr32>
2002-12-15 19:27:46 +0000

    Med protected attributes and methods public to make them available from code in other packages. Added method to build a whole complete set of properties with values diffrent from the defaults. Added a method to evaluate a ConnectionPoolDefinition against the default test property set.

--- src/java-test/org/logicalcobwebs/proxool/TestHelper.java
+++ src/java-test/org/logicalcobwebs/proxool/TestHelper.java
@@ -26,21 +26,21 @@ public class TestHelper {
 
     private static final String PASSWORD = "";
 
-    protected static final String PROXOOL_DRIVER = "org.logicalcobwebs.proxool.ProxoolDriver";
+    public static final String PROXOOL_DRIVER = "org.logicalcobwebs.proxool.ProxoolDriver";
 
-    protected static final String HYPERSONIC_DRIVER = "org.hsqldb.jdbcDriver";
+    public static final String HYPERSONIC_DRIVER = "org.hsqldb.jdbcDriver";
 
-    protected static final String HYPERSONIC_URL = "jdbc:hsqldb:test";
+    public static final String HYPERSONIC_URL = "jdbc:hsqldb:test";
 
     private static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
 
     private static final String ORACLE_URL = "jdbc:oracle:thin:@172.27.52.112:1527:hpgs1";
 
-    protected static final String SQL_INSERT_INTO_TEST = "INSERT INTO TEST VALUES(1);";
+    public static final String SQL_INSERT_INTO_TEST = "INSERT INTO TEST VALUES(1);";
 
-    protected static final String SQL_CHECK_TEST = "SELECT COUNT(*) FROM TEST;";
+    public static final String SQL_CHECK_TEST = "SELECT COUNT(*) FROM TEST;";
 
-    protected static Properties buildProperties() {
+    public static Properties buildProperties() {
         Properties info = new Properties();
         info.setProperty("user", USER);
         info.setProperty("password", PASSWORD);
@@ -48,37 +48,111 @@ public class TestHelper {
         return info;
     }
 
-    protected static void registerPool(String alias) throws SQLException {
+    /**
+     * Builds a complete set of proxool properties, with all values set to
+     * something different than the default vaule.
+     * @return the properties that was buildt.
+     */
+    public static Properties buildCompleteAlternativeProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("user", "sa");
+        properties.setProperty(ProxoolConstants.HOUSE_KEEPING_SLEEP_TIME_PROPERTY, "40000");
+        properties.setProperty(ProxoolConstants.HOUSE_KEEPING_TEST_SQL_PROPERTY, "select CURRENT_DATE");
+        properties.setProperty(ProxoolConstants.MAXIMUM_CONNECTION_COUNT_PROPERTY, "10");
+        properties.setProperty(ProxoolConstants.MINIMUM_CONNECTION_COUNT_PROPERTY, "3");
+        properties.setProperty(ProxoolConstants.MAXIMUM_CONNECTION_LIFETIME_PROPERTY, "18000000");
+        properties.setProperty(ProxoolConstants.MAXIMUM_NEW_CONNECTIONS_PROPERTY, "5");
+        properties.setProperty(ProxoolConstants.RECENTLY_STARTED_THRESHOLD_PROPERTY, "40000");
+        properties.setProperty(ProxoolConstants.OVERLOAD_WITHOUT_REFUSAL_LIFETIME_PROPERTY, "50000");
+        properties.setProperty(ProxoolConstants.MAXIMUM_ACTIVE_TIME_PROPERTY, "60000");
+        properties.setProperty(ProxoolConstants.VERBOSE_PROPERTY, "true");
+        properties.setProperty(ProxoolConstants.TRACE_PROPERTY, "true");
+        properties.setProperty(ProxoolConstants.FATAL_SQL_EXCEPTION_PROPERTY, "Fatal error");
+        properties.setProperty(ProxoolConstants.PROTOTYPE_COUNT_PROPERTY, "2");
+        return properties;
+    }
+
+    /**
+     * Test that the given ConnectionPoolDefinitionIF contains all the properties as defined in
+     * {@link #buildCompleteAlternativeProperties}.
+     * @param connectionPoolDefinition the ConnectionPoolDefinitionIF to be tested.
+     * @throws ProxoolException if any properties are missing or have the wrong value.
+     */
+    public static void equalsCompleteAlternativeProperties(ConnectionPoolDefinitionIF connectionPoolDefinition)
+        throws ProxoolException{
+        checkProperty("user", "sa", connectionPoolDefinition.getProperties().getProperty("user"));
+        checkProperty(ProxoolConstants.HOUSE_KEEPING_SLEEP_TIME, 40000,
+            connectionPoolDefinition.getHouseKeepingSleepTime());
+        checkProperty(ProxoolConstants.HOUSE_KEEPING_TEST_SQL, "select CURRENT_DATE",
+            connectionPoolDefinition.getHouseKeepingTestSql());
+        checkProperty(ProxoolConstants.MAXIMUM_CONNECTION_COUNT, 10,
+            connectionPoolDefinition.getMaximumConnectionCount());
+        checkProperty(ProxoolConstants.MINIMUM_CONNECTION_COUNT, 3,
+            connectionPoolDefinition.getMinimumConnectionCount());
+        checkProperty(ProxoolConstants.MAXIMUM_CONNECTION_LIFETIME, 18000000,
+            connectionPoolDefinition.getMaximumConnectionLifetime());
+        checkProperty(ProxoolConstants.MAXIMUM_NEW_CONNECTIONS, 5,
+            connectionPoolDefinition.getMaximumNewConnections());
+        checkProperty(ProxoolConstants.RECENTLY_STARTED_THRESHOLD, 40000,
+            connectionPoolDefinition.getRecentlyStartedThreshold());
+        checkProperty(ProxoolConstants.OVERLOAD_WITHOUT_REFUSAL_LIFETIME, 50000,
+            connectionPoolDefinition.getOverloadWithoutRefusalLifetime());
+        checkProperty(ProxoolConstants.MAXIMUM_ACTIVE_TIME, 60000,
+            connectionPoolDefinition.getMaximumActiveTime());
+        checkProperty(ProxoolConstants.VERBOSE, "true",
+            new Boolean(connectionPoolDefinition.isVerbose()).toString());
+        checkProperty(ProxoolConstants.TRACE, "true",
+            new Boolean(connectionPoolDefinition.isTrace() == true).toString());
+        checkProperty(ProxoolConstants.FATAL_SQL_EXCEPTION, "Fatal error",
+            connectionPoolDefinition.getFatalSqlExceptions().iterator().next().toString());
+        checkProperty(ProxoolConstants.PROTOTYPE_COUNT, 2,
+            connectionPoolDefinition.getPrototypeCount());
+    }
+    
+    private static void checkProperty(String name, String correctValue, String candidateValue) throws ProxoolException{
+        if (candidateValue == null) {
+            throw new ProxoolException(name + " was null.");
+        } else if (! candidateValue.equals(correctValue)) {
+            throw new ProxoolException("Expected value for " + name + " was " + correctValue + " but the value was " +
+                candidateValue + ".");
+        }
+    }
+
+    private static void checkProperty(String name, int correctValue, int candidateValue) throws ProxoolException{
+            checkProperty(name, String.valueOf(correctValue), String.valueOf(candidateValue));
+    }
+
+    public static void registerPool(String alias) throws SQLException {
         registerPool(alias, TestHelper.buildProperties());
     }
 
-    protected static void registerPool(String alias, Properties info) throws SQLException {
+    public static void registerPool(String alias, Properties info) throws SQLException {
         ProxoolFacade.registerConnectionPool(getFullUrl(alias), info);
     }
 
-    protected static void registerOraclePool(String alias, Properties info) throws SQLException {
+    public static void registerOraclePool(String alias, Properties info) throws SQLException {
         ProxoolFacade.registerConnectionPool(getFullOracleUrl(alias), info);
     }
 
-    protected static Connection getProxoolConnection(String url) throws ClassNotFoundException, SQLException {
+    public static Connection getProxoolConnection(String url) throws ClassNotFoundException, SQLException {
         return getProxoolConnection(url, buildProperties());
     }
 
-    protected static Connection getProxoolConnection(String url, Properties info) throws ClassNotFoundException, SQLException {
+    public static Connection getProxoolConnection(String url, Properties info) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         Class.forName(PROXOOL_DRIVER);
         connection = DriverManager.getConnection(url, info);
         return connection;
     }
 
-    protected static String getSimpleUrl(String alias) {
+    public static String getSimpleUrl(String alias) {
         String url = ProxoolConstants.PROXOOL
                 + ProxoolConstants.ALIAS_DELIMITER
                 + alias;
         return url;
     }
 
-    protected static String buildProxoolUrl(String alias, String driver, String delegateUrl) {
+    public static String buildProxoolUrl(String alias, String driver, String delegateUrl) {
         String url = ProxoolConstants.PROXOOL
                 + ProxoolConstants.ALIAS_DELIMITER
                 + alias
@@ -89,7 +163,7 @@ public class TestHelper {
         return url;
     }
 
-    protected static String getFullUrl(String alias) {
+    public static String getFullUrl(String alias) {
         String url = ProxoolConstants.PROXOOL
                 + ProxoolConstants.ALIAS_DELIMITER
                 + alias
@@ -100,7 +174,7 @@ public class TestHelper {
         return url;
     }
 
-    protected static String getFullOracleUrl(String alias) {
+    public static String getFullOracleUrl(String alias) {
         try {
             Class.forName(ORACLE_DRIVER);
         } catch (ClassNotFoundException e) {
@@ -116,14 +190,14 @@ public class TestHelper {
         return url;
     }
 
-    protected static Connection getDirectConnection() throws ClassNotFoundException, SQLException {
+    public static Connection getDirectConnection() throws ClassNotFoundException, SQLException {
         Connection connection = null;
         Class.forName(HYPERSONIC_DRIVER);
         connection = DriverManager.getConnection(HYPERSONIC_URL, buildProperties());
         return connection;
     }
 
-    protected static Connection getDirectOracleConnection() throws ClassNotFoundException, SQLException {
+    public static Connection getDirectOracleConnection() throws ClassNotFoundException, SQLException {
         Connection connection = null;
         Class.forName(ORACLE_DRIVER);
         Properties info = buildProperties();
@@ -133,7 +207,7 @@ public class TestHelper {
         return connection;
     }
 
-    protected static int getCount(Connection connection, String table) throws SQLException {
+    public static int getCount(Connection connection, String table) throws SQLException {
         Statement statement = null;
         ResultSet resultSet = null;
         int count = -1;
@@ -162,7 +236,7 @@ public class TestHelper {
         return count;
     }
 
-    protected static void execute(Connection connection, String sql) throws SQLException {
+    public static void execute(Connection connection, String sql) throws SQLException {
         Statement statement = null;
         try {
             statement = connection.createStatement();
@@ -180,7 +254,7 @@ public class TestHelper {
         }
     }
 
-    protected static void createTable(String table) throws Exception {
+    public static void createTable(String table) throws Exception {
         try {
             execute(getDirectConnection(), "CREATE TABLE " + table + " (A INT)");
         } catch (Exception e) {
@@ -189,15 +263,15 @@ public class TestHelper {
         }
     }
 
-    protected static void dropTable(String table) throws SQLException, ClassNotFoundException {
+    public static void dropTable(String table) throws SQLException, ClassNotFoundException {
         execute(getDirectConnection(), "DROP TABLE " + table);
     }
 
-    protected static void insertRow(Connection connection, String table) throws SQLException {
+    public static void insertRow(Connection connection, String table) throws SQLException {
         execute(connection, "INSERT INTO " + table + " VALUES(1)");
     }
 
-    protected static void testConnection(Connection connection) throws SQLException {
+    public static void testConnection(Connection connection) throws SQLException {
         execute(connection, "SELECT SYSDATE FROM DUAL");
     }
 
