ea95ba1c1125ea75c7a84d0305c8c65bc4583e76
billhorsman <billhorsman>
2003-01-14 23:50:59 +0000

    keeps track of version

---
+++ src/java/org/logicalcobwebs/proxool/Version.java
@@ -0,0 +1,56 @@
+/*
+ * This software is released under the Apache Software Licence. See
+ * package.html for details. The latest version is available at
+ * http://proxool.sourceforge.net
+ */
+package org.logicalcobwebs.proxool;
+
+/**
+ * Tells you the version. You can tell what sort of release it is
+ * from the version. For instance:
+ *
+ * 1.0 (1 January
+ *   A proper released binary file at version 1.0.
+ *
+ * 1.0+
+ *   Built from the source based on version 1,0 (but there is no
+ *   way of knowing whether the source has been altered).
+ *
+ * 1.0.2003.1.2
+ *   A snapshot release built on January 2nd. This comes after the
+ *   version 1.0 and before 1.1.
+ *
+ * @version $Revision: 1.1 $, $Date: 2003/01/14 23:50:59 $
+ * @author bill
+ * @author $Author: billhorsman $ (current maintainer)
+ * @since Proxool 0.6
+ */
+public class Version {
+
+    /**
+     * This is changed by the Ant script when you build from the
+     * source code.
+     */
+    private static final String RELEASE = null;
+
+    public static String getVersion() {
+        if (RELEASE != null) {
+            return RELEASE;
+        } else {
+            /**
+             * This means that we haven't used the Ant script so this
+             * is just our best guess at the version.
+             */
+            return "0.5+";
+        }
+    }
+
+}
+
+/*
+ Revision history:
+ $Log: Version.java,v $
+ Revision 1.1  2003/01/14 23:50:59  billhorsman
+ keeps track of version
+
+ */
