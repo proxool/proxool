97d6e4b9ad66e3a2af4c4cd8d698e9f77004be3c
billhorsman <billhorsman>
2003-01-23 11:41:56 +0000

    doc

--- src/java/org/logicalcobwebs/proxool/ConfigurationListenerIF.java
+++ src/java/org/logicalcobwebs/proxool/ConfigurationListenerIF.java
@@ -8,20 +8,15 @@ package org.logicalcobwebs.proxool;
 import java.util.Properties;
 
 /**
- * Configures a {@link ConnectionPoolDefinitionIF definition}.
- * and listens to any changes made to it. This gives you the
- * opportunity to persist any changes made to it.
+ * Listens to any changes made to a {@link ConnectionPoolDefinitionIF definition}.
+ * This gives you the opportunity to persist a definition.
  *
- * One of the tasks for a configurator is to
- * {@link ProxoolFacade#registerConnectionPool(java.lang.String, java.util.Properties, org.logicalcobwebs.proxool.ConfigurationListenerIF) register}
- * the pool. When it does this it should pass in a reference to this configurator. This ensures two things:
+ * After registering a pool you should call
+ * {@link ProxoolFacade#setConfigurationListener setConfigurationListener}.
+ * This ensures that any changes to the definition will call this object's
+ * {@link #defintionUpdated defintionUpdated} method
  *
- * 1) Any changes to the definition will call this object's {@link #defintionUpdated} method
- *
- * 2) Any on-the-fly updates to the definition (i.e. when passed in as a Properties object to
- * the driver) will result in a warning log message,
- *
- * @version $Revision: 1.1 $, $Date: 2003/01/18 15:12:23 $
+ * @version $Revision: 1.2 $, $Date: 2003/01/23 11:41:56 $
  * @author billhorsman
  * @author $Author: billhorsman $ (current maintainer)
  * @since Proxool 0.6
@@ -42,6 +37,9 @@ public interface ConfigurationListenerIF {
 /*
  Revision history:
  $Log: ConfigurationListenerIF.java,v $
+ Revision 1.2  2003/01/23 11:41:56  billhorsman
+ doc
+
  Revision 1.1  2003/01/18 15:12:23  billhorsman
  renamed ConfiguratorIF to ConfigurationListenerIF to better reflect role
 
