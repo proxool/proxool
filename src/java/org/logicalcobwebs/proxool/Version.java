ec26af7709649e27377c34c06fc7dfc0ed8c9175
billhorsman <billhorsman>
2003-02-12 00:50:34 +0000

    change the CVS version to be x.y+ (a bit more informative)

--- src/java/org/logicalcobwebs/proxool/Version.java
+++ src/java/org/logicalcobwebs/proxool/Version.java
@@ -23,7 +23,7 @@ import org.logicalcobwebs.logging.LogFactory;
  *   A snapshot release built on January 2nd. This comes after the
  *   version 1.0 and before 1.1.
  *
- * @version $Revision: 1.5 $, $Date: 2003/02/06 17:41:05 $
+ * @version $Revision: 1.6 $, $Date: 2003/02/12 00:50:34 $
  * @author bill
  * @author $Author: billhorsman $ (current maintainer)
  * @since Proxool 0.6
@@ -40,7 +40,7 @@ public class Version {
 
     private static final String BUILD_DATE = null;
 
-    private static final String CVS = "cvs";
+    private static final String CVS = "0.6+";
 
     public static String getVersion() {
         StringBuffer version = new StringBuffer();
@@ -77,6 +77,9 @@ public class Version {
 /*
  Revision history:
  $Log: Version.java,v $
+ Revision 1.6  2003/02/12 00:50:34  billhorsman
+ change the CVS version to be x.y+ (a bit more informative)
+
  Revision 1.5  2003/02/06 17:41:05  billhorsman
  now uses imported logging
 
