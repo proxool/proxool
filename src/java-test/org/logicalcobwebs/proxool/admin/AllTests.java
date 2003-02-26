454bb1bd6e720b99d8c578975ac2265c261095c5
chr32 <chr32>
2003-02-26 19:03:08 +0000

    Added suite for JMX tests.

--- src/java-test/org/logicalcobwebs/proxool/admin/AllTests.java
+++ src/java-test/org/logicalcobwebs/proxool/admin/AllTests.java
@@ -13,9 +13,9 @@ import org.logicalcobwebs.proxool.GlobalTest;
 /**
  * Run all in admin package tests
  *
- * @version $Revision: 1.2 $, $Date: 2003/02/26 11:53:17 $
+ * @version $Revision: 1.3 $, $Date: 2003/02/26 19:03:08 $
  * @author Bill Horsman (bill@logicalcobwebs.co.uk)
- * @author $Author: billhorsman $ (current maintainer)
+ * @author $Author: chr32 $ (current maintainer)
  * @since Proxool 0.7
  */
 public class AllTests {
@@ -29,6 +29,7 @@ public class AllTests {
         suite.addTestSuite(StatisticsListenerTest.class);
         suite.addTestSuite(StatisticsTest.class);
         suite.addTestSuite(SnapshotTest.class);
+        suite.addTest(org.logicalcobwebs.proxool.admin.jmx.AllTests.suite());
 
         // create a wrapper for global initialization code.
         TestSetup wrapper = new TestSetup(suite) {
@@ -44,6 +45,9 @@ public class AllTests {
 /*
  Revision history:
  $Log: AllTests.java,v $
+ Revision 1.3  2003/02/26 19:03:08  chr32
+ Added suite for JMX tests.
+
  Revision 1.2  2003/02/26 11:53:17  billhorsman
  added StatisticsTest and SnapshotTest
 
