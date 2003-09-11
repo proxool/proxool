067a5825fcaa14a25e5312718944e70df1f840e4
billhorsman <billhorsman>
2003-09-11 23:00:53 +0000

    *** empty log message ***

---
+++ src/java-test/org/logicalcobwebs/logging/Jdk14LoggerTest.java
@@ -0,0 +1,49 @@
+/*
+ * This software is released under a licence similar to the Apache Software Licence.
+ * See org.logicalcobwebs.proxool.package.html for details.
+ * The latest version is available at http://proxool.sourceforge.net
+ */
+package org.logicalcobwebs.logging;
+
+import org.logicalcobwebs.proxool.AbstractProxoolTest;
+import org.logicalcobwebs.proxool.ConnectionPoolTests;
+
+/**
+ * Test {@link org.logicalcobwebs.logging.impl.Jdk14Logger}
+ *
+ * @version $Revision: 1.1 $, $Date: 2003/09/11 23:00:53 $
+ * @author billhorsman
+ * @author $Author: billhorsman $ (current maintainer)
+ * @since Proxool 0.8
+ */
+public class Jdk14LoggerTest extends AbstractProxoolTest {
+
+    private static final Log LOG = LogFactory.getLog(ConnectionPoolTests.class);
+
+    public Jdk14LoggerTest(String alias) {
+        super(alias);
+    }
+
+    /**
+     * Override {@link AbstractProxoolTest#setUp} because we don't want to
+     * configure logging.
+     * @throws Exception if anything goes wrong (it can't, we don't do anything)
+     */
+    protected void setUp() throws Exception {
+        // Don't setup logging. Just use the default JDK14Logger
+    }
+
+    /**
+     * Override {@link AbstractProxoolTest#tearDown} because it does things that rely
+     * on {@link AbstractProxoolTest#setUp} having run
+     * @throws Exception if anything goes wrong (it can't, we don't do anything)
+     */
+    protected void tearDown() throws Exception {
+        // Don't do anything
+    }
+
+    public void testJdk14Logger() {
+        LOG.info("Does this log correctly?");
+    }
+
+}
