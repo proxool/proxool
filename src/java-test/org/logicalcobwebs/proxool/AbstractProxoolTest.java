99ee8213119735ea1323f4d8d824a490db36af46
billhorsman <billhorsman>
2003-09-30 19:09:46 +0000

    Now uses a readwrite lock to make sure that each test runs sequentially. This should be true all the time, but sometimes tests fail and it is always because of some timing issue that is very hard to track down. This is an attempt to fix that.

--- src/java-test/org/logicalcobwebs/proxool/AbstractProxoolTest.java
+++ src/java-test/org/logicalcobwebs/proxool/AbstractProxoolTest.java
@@ -8,12 +8,13 @@ package org.logicalcobwebs.proxool;
 import junit.framework.TestCase;
 import org.logicalcobwebs.logging.Log;
 import org.logicalcobwebs.logging.LogFactory;
+import org.logicalcobwebs.concurrent.ReaderPreferenceReadWriteLock;
 
 import java.util.Stack;
 
 /**
  * Provides common code for all Proxool tests
- * @version $Revision: 1.3 $, $Date: 2003/03/04 10:11:09 $
+ * @version $Revision: 1.4 $, $Date: 2003/09/30 19:09:46 $
  * @author bill
  * @author $Author: billhorsman $ (current maintainer)
  * @since Proxool 0.8
@@ -23,6 +24,8 @@ public abstract class AbstractProxoolTest extends TestCase {
     private static final Log LOG = LogFactory.getLog(AbstractProxoolTest.class);
 
     private String alias;
+
+    private static ReaderPreferenceReadWriteLock testLock = new ReaderPreferenceReadWriteLock();
 
     private Stack threadNames = new Stack();
 
@@ -36,6 +39,7 @@ public abstract class AbstractProxoolTest extends TestCase {
      */
     protected void setUp() throws Exception {
         GlobalTest.globalSetup();
+        testLock.writeLock().acquire();
         threadNames.push(Thread.currentThread().getName());
         LOG.debug("Thread '" + Thread.currentThread().getName() + "' -> '" + alias + "'");
         Thread.currentThread().setName(alias);
@@ -46,6 +50,7 @@ public abstract class AbstractProxoolTest extends TestCase {
      */
     protected void tearDown() throws Exception {
         GlobalTest.globalTeardown(alias);
+        testLock.writeLock().release();
         Thread.currentThread().setName((String) threadNames.pop());
         LOG.debug("Thread '" + alias + "' -> '" + Thread.currentThread().getName() + "'");
     }
@@ -56,6 +61,11 @@ public abstract class AbstractProxoolTest extends TestCase {
 /*
  Revision history:
  $Log: AbstractProxoolTest.java,v $
+ Revision 1.4  2003/09/30 19:09:46  billhorsman
+ Now uses a readwrite lock to make sure that each test runs sequentially. This should be true all the time, but sometimes
+ tests fail and it is always because of some timing issue that is very hard to track down. This is an attempt to
+ fix that.
+
  Revision 1.3  2003/03/04 10:11:09  billhorsman
  actually made abstract
 
