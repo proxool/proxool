4a4573790c015afd732087f0f540ef36d7c29932
billhorsman <billhorsman>
2003-10-26 15:23:30 +0000

    0.8.0

--- src/java/org/logicalcobwebs/proxool/Version.java
+++ src/java/org/logicalcobwebs/proxool/Version.java
@@ -22,7 +22,7 @@ import org.logicalcobwebs.logging.LogFactory;
  * 1.0.1 (2 January)
  *   A bug fix release built on January 2nd.
  *
- * @version $Revision: 1.14 $, $Date: 2003/10/01 19:31:26 $
+ * @version $Revision: 1.15 $, $Date: 2003/10/26 15:23:30 $
  * @author bill
  * @author $Author: billhorsman $ (current maintainer)
  * @since Proxool 0.6
@@ -39,7 +39,7 @@ public class Version {
 
     private static final String BUILD_DATE = null;
 
-    private static final String CVS = "0.8.0RC2+";
+    private static final String CVS = "0.8.0+";
 
     public static String getVersion() {
         StringBuffer version = new StringBuffer();
@@ -76,6 +76,9 @@ public class Version {
 /*
  Revision history:
  $Log: Version.java,v $
+ Revision 1.15  2003/10/26 15:23:30  billhorsman
+ 0.8.0
+
  Revision 1.14  2003/10/01 19:31:26  billhorsman
  0.8.0RC2
 
