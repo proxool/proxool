fd6b91f0480bae9f23ba4d611383d0492513f53d
billhorsman <billhorsman>
2006-03-09 10:45:59 +0000

    Include Cglib using Jarjar instead of in source code.

---
+++ jarjar/readme.txt
@@ -0,0 +1,2 @@
+Cglib 2.1_3 has been patched so that it can be correctly processed by JarJar. See
+cglib-nodep-2.1_3-patch-BH1.patch for details.  
--- src/java/org/logicalcobwebs/cglib/core/Customizer.java
+++
@@ -1,7 +0,0 @@
-package org.logicalcobwebs.cglib.core;
-
-import org.logicalcobwebs.asm.Type;
-
-public interface Customizer {
-    void customize(CodeEmitter e, Type type);
-}
